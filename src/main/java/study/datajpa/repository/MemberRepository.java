7a1dac9c0fa98793a24825b3d745957ae1fb38fd
limsh <sehee167@naver.com>
2023-02-05 09:30:59 +0900

    @EntityGraph (join fetch)

--- src/main/java/study/datajpa/repository/MemberRepository.java
+++ src/main/java/study/datajpa/repository/MemberRepository.java
@@ -2,10 +2,13 @@ package study.datajpa.repository;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import study.datajpa.entity.Member;
+
+import java.util.List;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
 
@@ -18,4 +21,14 @@ public interface MemberRepository extends JpaRepository<Member, Long> {
     @Modifying(clearAutomatically = true) // 벌크 연산은 영속성 컨텍스트를 무시하고 실행하기 때문에 쿼리 실행 후 영속성 컨텍스트를 초기화 필요
     @Query("update Member m set m.age = m.age + 1 where m.age >= 20")
     int bulkAgePlus(int age);
+
+    @Query("select m from Member m left join fetch m.team")
+    List<Member> findMemberFetchJoin();
+
+    @EntityGraph(attributePaths = {"team"})
+    @Query("select m from Member m")
+    List<Member> findMemberEntityGraph();
+
+    @EntityGraph(attributePaths = {"team"})
+    List<Member> findByUsername(String username);
 }
