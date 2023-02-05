ab55384827a5a27724c19440936075c0b95ec67a
limsh <sehee167@naver.com>
2023-02-05 12:14:01 +0900

    사용자 정의 리포지토리 구현 클래스명 최신 버전으로 변경

--- src/main/java/study/datajpa/repository/MemberRepositoryCustomImpl.java
+++ src/main/java/study/datajpa/repository/MemberRepositoryCustomImpl.java
@@ -7,7 +7,7 @@ import study.datajpa.entity.Member;
 import java.util.List;
 
 @RequiredArgsConstructor
-public class MemberRepositoryImpl implements MemberRepositoryCustom { // 네이밍 규칙 : MemberRepository + Impl
+public class MemberRepositoryCustomImpl implements MemberRepositoryCustom { // 네이밍 규칙 : MemberRepository + Impl
 
     private final EntityManager em;
 
